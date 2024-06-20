4ca38a1020081a0ac3ccd621918ead940a74dfc4
Aman Dewett <amandewett@gmail.com>
2024-06-20 14:17:52 -0400

    updates
    - fixed error handlers

--- src/middleware/errorHandlers.ts
+++ src/middleware/errorHandlers.ts
@@ -1,14 +1,10 @@
 import { Request, Response, NextFunction, ErrorRequestHandler } from "express";
 
-const errorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const statusCode = req.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
+const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.status || 500;
+  const errorMessage = err.message || `Internal server error`;
+
+  res.status(statusCode).json({
     status: false,
     message: err.message,
     stack: process.env.ENV === "Production" ? null : err.stack,
